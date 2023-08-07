587f0d8da3b8f1af11583e615bc6122c0f8cf732
Aadityagoel15git config --global user.name Aadityagoel15git config --global user.name Aadityagoel15 <goelaaditya2003@gmail.com>
2023-08-07 20:45:24 +0530

    Changes

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -8,7 +8,7 @@ export class NewsItem extends Component {
         <div className="card">
           <span class="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left:'90%', zIndex:'1'}}>{source}
           </span>
-          <img src={imageUrl?imageUrl:"https://st2.depositphotos.com/2619903/6028/v/450/depositphotos_60287149-stock-illustration-no-image-signs-for-web.jpg"} className="card-img-top" alt="..."/>
+          <img src={imageUrl?imageUrl:"https://t3.ftcdn.net/jpg/00/36/94/26/360_F_36942622_9SUXpSuE5JlfxLFKB1jHu5Z07eVIWQ2W.jpg"} className="card-img-top" alt="..."/>
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description?description:"Click on Read More to read news"}...</p>
